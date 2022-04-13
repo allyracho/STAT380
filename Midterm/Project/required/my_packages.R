b514d084b21ecbed9cb51429aa81dcfcbe96f594
Ally Racho <55199345+allyracho@users.noreply.github.com>
2022-04-12 20:32:46 -0400

    Add files via upload

---
+++ Midterm/Project/required/my_packages.R
@@ -0,0 +1,8 @@
+## Ally Racho
+## STAT380
+## NCAA Tournament -- Midterm Project
+## Needed Packages
+
+install.packages("data.table")
+install.packages("glmnet")
+install.packages("Metrics")
---
+++ Midterm/run_project.R
@@ -0,0 +1,4 @@
+source("./project/src/features/Midterm_MasseyOrd.R")
+source("./project/src/features/Midterm_RegularSeasTourney.R")
+source("./project/src/features/Midterm_merge_stats_MO.R")
+source("./project/src/models/Midterm_Model.R")
